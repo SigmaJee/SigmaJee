322cccaba0b8f29b63847ce6765cf704b3baf227
Raghav <raghavvohra375@gmail.com>
2025-07-04 20:26:33 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,7 @@ import { useAuth } from "./LoginContext/loginContext.jsx";
 import { SignupAuth } from "./components/CanGoSignup/canSignupContext.jsx";
 import CreateTest from "./Test/CreateTest/CreateTest.jsx";
 import TestPage from "./Test/TestPage/TestPage.jsx";
-import Untest from "./Test/UnattTest/Untest.jsx";
 import TestScreen from "./Test/Screen/TestScreen.jsx";
-import Attest from "./Test/AttemptedTest/Attempted.jsx";
 import CreatedTests from "./Test/CreatedTest/CreatedTests.jsx";
 function App() {
    const location = useLocation(); 
@@ -52,9 +50,7 @@ function App() {
       {toast && <Toast elements={elements} />}
       {loading && <Loading />}
       <Routes>
-         <Route path='/att-test' element={user?<Attest />:<Fullpage elements={elements} func={func} funcs={funcs}/>} />
          <Route path='/test-screen' element={user?<TestScreen />:<Fullpage elements={elements} func={func} funcs={funcs}/>} />
-         <Route path='/untest' element={user?<Untest />:<Fullpage elements={elements} func={func} funcs={funcs}/>} />
           <Route path='/test-page' element={user?<TestPage />:<Fullpage elements={elements} func={func} funcs={funcs}/>} />
           <Route path='/test-create' element={user?<CreateTest />:<Fullpage elements={elements} func={func} funcs={funcs}/>} />
           <Route path='/test-created' element={user?<CreatedTests />:<Fullpage elements={elements} func={func} funcs={funcs}/>} />
