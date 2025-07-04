aeee7e69d489a7e6aae8e1106562e1e396aa6990
Raghav <raghavvohra375@gmail.com>
2025-07-04 20:25:23 +0530

    a

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { LoginProvider } from './LoginContext/loginContext.jsx';
 import { CanSignupContext } from './components/CanGoSignup/canSignupContext.jsx';
 import { UserProvider } from './UserContext/userContext.jsx';
-import { TestContext } from './Test/TestContext/TestContext.jsx';
   createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <TestContext>
         <LoginProvider>
           <CanSignupContext>
             <App />
           </CanSignupContext>
         </LoginProvider>
-        </TestContext>
       </UserProvider>
     </BrowserRouter>
   </StrictMode>
